f6c30fc1c309eeffda967031a245005a6448a7ce
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-09-10 13:22:15 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Oleg Gavashi on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
